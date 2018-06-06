8fed7a01bff72507a945154c1a09467b7e9722ed
JoshuaManto <joshuamanto@yahoo.com>
2018-06-06 16:25:23 +0800

    Add Greet new user and inform/alert others

--- server/server.js
+++ server/server.js
@@ -27,11 +27,33 @@ io.on('connection', (socket) =>
   //
   // });
 
+  // soecket.emit from Admin text Welcome to the chat app
+  socket.emit('newMessage',
+  {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+  // socket.broadcast.emit from Admin text new user joined
+  socket.broadcast.emit('newMessage',
+  {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (message) =>
   {
     console.log('createMessage', message);
     // emits to all connections
-    io.emit('newMessage',
+    // io.emit('newMessage',
+    // {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+
+    socket.broadcast.emit('newMessage',
     {
       from: message.from,
       text: message.text,
