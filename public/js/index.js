4df230b04e25c36ff1159b7bd0d5826197c9cdbf
JoshuaManto <joshuamanto@yahoo.com>
2018-06-06 21:00:20 +0800

    Add geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -30,6 +30,17 @@ socket.on('newMessage', function (message)
 //   console.log('Got it', data);
 // });
 
+socket.on('newLocationMessage', function (message)
+{
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target="_blank">My current location</a>');
+
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+});
+
 jQuery('#message-form').on('submit', function (e)
 {
   e.preventDefault();
@@ -42,4 +53,30 @@ jQuery('#message-form').on('submit', function (e)
   {
 
   });
+});
+
+// GEO LOCATION
+var locationButton = jQuery('#send-Location');
+locationButton.on('click', function ()
+{
+  if(!navigator.geolocation)
+  {
+    // can change this alert if you use bootstrap or foundation or another type of frontend api / framework
+    return alert('Geolocation not supported by your browser');
+  }
+
+  navigator.geolocation.getCurrentPosition(function (position)
+  {
+    socket.emit('createLocationMessage',
+    {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    })
+  }, function ()
+  {
+    alert('Unable to fetch location');
+  });
+
+
+
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message')
+var {generateMessage, generateLocationMessage} = require('./message')
 
 describe('generateMessage', () =>
 {
@@ -17,6 +17,23 @@ describe('generateMessage', () =>
     // assert text match
     expect(message.from).toBe(from);
     expect(message.text).toBe(text);
+
+  });
+});
+
+describe('geenrateLocationMessage', () =>
+{
+  it('should generate correct location object', () =>
+  {
+    var from = 'Admin';
+    var latitude = 50;
+    var longitude = 25;
+    var url = 'https://www.google.com/maps?q=50,25';
+    var message = generateLocationMessage(from, latitude, longitude);
+
+    expect(typeof message.createdAt).toBe('number');
+    expect(message.from).toBe(from);
+    expect(message.url).toBe(url);
 
   });
 });
